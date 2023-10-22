c22763024361a3d0a3977efb8b24e32583ec9a0e
inhasid <sidorchuk.inga@gmail.com>
2023-10-22 11:06:30 +0200

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submin', (event) => {
+    event.preventDefault();
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
 
+    if (emailInput.value !== '' && passwordInput.value !== '') {
+        const formData = {
+            email: emailInput.value,
+            password: passwordInput.value,
+        };
+        console.log(formData);
+    } else {
+        alert('Всі поля повинні бути заповнені'); 
+    }
+
+    event.currentTarget.reset();
+});
